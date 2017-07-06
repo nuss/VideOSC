27d7dea5e4a6a80982756b3ca8c01ee18630fa1c
Stefan Nussbaumer <st9fan@gmail.com>
2017-07-07 00:27:16 +0200

    memory leak testing
    needs at least API level 21 - not for production
    
    Signed-off-by: Stefan Nussbaumer <st9fan@gmail.com>

---
+++ app/src/main/java/net/videosc/VideOSCApplication.java
@@ -0,0 +1,23 @@
+package net.videosc;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * Created by stefan on 06.07.17.
+ */
+
+public class VideOSCApplication extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		if (LeakCanary.isInAnalyzerProcess(this)) {
+			// This process is dedicated to LeakCanary for heap analysis.
+			// You should not init your app in this process.
+			return;
+		}
+		LeakCanary.install(this);
+		// Normal app init code...
+	}
+}
